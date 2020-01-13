c75f7844fe35b5d39db141fb5948a5a801f704c3
jorra04 <jorra04@my.yorku.ca>
2020-01-13 14:27:12 -0500

    first commit

--- src/main/java/venn/Main.java
+++ src/main/java/venn/Main.java
@@ -3,7 +3,7 @@ package venn;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("hello world");
+		System.out.println("hello world 1");
 		// TODO Auto-generated method stub
 
 	}
