c716f59e220f122edab035f7a6b26accb2d96779
jorra04 <jorra04@my.yorku.ca>
2020-01-20 11:56:13 -0500

    fixed errors

--- src/main/java/venn/Main.java
+++ src/main/java/venn/Main.java
@@ -5,7 +5,7 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("hello world 3");
 
-		System.out.println("Brian changed this");\
+		System.out.println("Brian changed this");
 		// TODO Auto-generated method stub
 
 	}
