da277f9ffab62f10cff00cd466862ace014dc322
jorra04 <jorra04@my.yorku.ca>
2020-01-20 12:05:15 -0500

    made changes

--- src/main/java/venn/Main.java
+++ src/main/java/venn/Main.java
@@ -5,7 +5,7 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("hello world 3");
 
-		System.out.println("Brian changed this");
+		System.out.println("jorra changed this part 1");
 		
 		System.out.println("Jorra changed this");
 		// TODO Auto-generated method stub
